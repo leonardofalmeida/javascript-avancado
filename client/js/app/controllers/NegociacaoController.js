75318c0b39d848803dae0d0862819f0e2dbe7202
leonardofalmeida <ferreiraleoalmeida@gmail.com>
2019-05-08 20:46:39 -0300

    Importando negociacoes automaticamente, movido não atributos da classe, para o metodo _init

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -25,7 +25,10 @@ class NegociacaoController {
       new MensagemView($("#mensagemView")),
       "texto"
     );
+    this._init();
+  }
 
+  _init() {
     ConnectionFactory.getConnection()
       .then(connection => new NegociacaoDao(connection))
       .then(dao => dao.listaTodos())
@@ -38,6 +41,9 @@ class NegociacaoController {
         console.log(error);
         this._mensagem.texto = error;
       });
+    setInterval(() => {
+      this.importaNegociacoes();
+    }, 3000);
   }
 
   ordena(coluna) {
